412ec90cfc2201666540c7f6326e5ca2802c27c1
allat mouad <80215500+moallat@users.noreply.github.com>
2022-10-18 18:11:14 +0100

    gateway config

---
+++ micro-services-spring-cloud/micro-service-gateway/src/test/java/ma/enset/microservicegateway/MicroServiceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.microservicegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
