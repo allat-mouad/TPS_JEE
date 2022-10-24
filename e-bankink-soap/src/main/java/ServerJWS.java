6861f40845cb3e9c5d992467da9f3884d90481aa
allat mouad <80215500+moallat@users.noreply.github.com>
2022-10-24 09:50:13 +0100

    creating http server

--- e-bankink-soap/src/main/java/ServerJWS.java
+++ e-bankink-soap/src/main/java/ServerJWS.java
@@ -1,5 +1,7 @@
+import jakarta.xml.ws.Endpoint;
+import ws.BanqueService;
 public class ServerJWS {
     public static void main(String[] args) {
-
+        Endpoint.publish("http://localhost:9191/",new BanqueService());
     }
 }
